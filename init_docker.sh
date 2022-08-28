797d8728383a416b77c411fb4c94d7cdfa48dd13
xuxin <734532469@qq.com>
2022-08-28 20:46:06 +0800

    add init_docker

---
+++ init_docker.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install vim -y
+
+apt-get install zsh -y
+
+chsh -s /bin/zsh
+
+apt-get install wget -y
+
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+
+bash
+
+zsh
+
+
+
+
+#!/bin/bash
+
+apt-get update
+
+apt-get install vim -y
+
+
+# init zsh and oh my zsh
+
+apt-get install zsh -y
+apt-get install git -y
+chsh -s /bin/zsh
+apt-get install wget -y
+wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+zsh
+
+# 🖼️ install the GUI program
+# more infomation come from here : 📚 https://gist.github.com/sorny/969fe55d85c9b0035b0109a31cbcb088
+
+apt-get install xserver-xorg x11-xserver-utils -y
+
+
+# 💻 host should exec this command
+# xhost +
+
+# 😕 this step still can setting when docker start like this
+# eg: docker run -e DISPLAY=$ip:0 sshipway/xclock
+# ‼️  this ip is the host ip , isn`t the docker container ip
+
+ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
+export DISPLAY=$ip:0.0
+
+
+
+
+
