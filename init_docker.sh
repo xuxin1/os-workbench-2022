ee319bb3934570bbd9c9e5c2593fd62f1bcc8b30
xuxin <734532469@qq.com>
2022-08-29 20:41:36 +0800

    fix init_docker.sh

--- init_docker.sh
+++ init_docker.sh
@@ -4,42 +4,16 @@ apt-get update
 
 apt-get install vim -y
 
-<<<<<<< Updated upstream
-apt-get install zsh -y
-
-chsh -s /bin/zsh
-
-apt-get install wget -y
-
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-
-bash
-
-zsh
-
-
-
-
-#!/bin/bash
-
-apt-get update
-
-apt-get install vim -y
-
-=======
->>>>>>> Stashed changes
-
 # init zsh and oh my zsh
 
 apt-get install zsh -y
 apt-get install git -y
 chsh -s /bin/zsh
 apt-get install wget -y
-<<<<<<< Updated upstream
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
-=======
+
 # wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
->>>>>>> Stashed changes
+
 zsh
 
 # 🖼️ install the GUI program
@@ -57,7 +31,6 @@ apt-get install xserver-xorg x11-xserver-utils -y
 
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 export DISPLAY=$ip:0.0
-
 
 
 
