9208447e6e493ba1b1fd5b55a19d98de51c85c2c
xuxin <734532469@qq.com>
2022-08-31 15:26:22 +0800

    pstree v1

---
+++ README.md
@@ -0,0 +1,34 @@
+# 实验 M1 🧪
+
+* 实验地址(https://jyywiki.cn/OS/2022/labs/M1)
+
+## 实验资料 📚
+* linux /proc 文件详解 (https://jyywiki.cn/OS/2022/labs/M1)
+* c 语言 file 操作 (https://blog.csdn.net/zhanghaiyang9999/article/details/107032563)
+* c 语言 file dir 操作 (https://blog.csdn.net/dream_allday/article/details/75243818)
+* 
+
+ ## 实验目的 🎯
+ 
+ * 实现ps 命令
+ * Linux 系统中可以同时运行多个程序。运行的程序称为进程。除了所有进程的根之外，每个进程都有它唯一的父进程，你的任务就是把这棵树在命令行中输出。你可以自由选择展示树的方式 (例如使用缩进表示父子关系，这是最容易的)。
+## 预期结果
+* `pstree [OPTION]…`
+  * `-p, --show-pids: 打印每个进程的进程号。`
+  * `-n --numeric-sort: 按照pid的数值从小到大顺序输出一个进程的直接孩子。`
+  * `-V --version: 打印版本信息。`
+## 实验步骤
+
+- 获取进程号
+  - 读取 /proc 目录下的 数字编号文件夹名称（数字为进程的PID）
+  - 读取 /proc/number/status 文件中的信息 （获取PID 和 PPID）
+- 通过 PID 和 PPID 建立进程间的相关联系
+- 构建进程树 (按照pid)
+- 实现 CLI 参数解析
+
+## 实验问题 🤔
+
+* [ ] 你的程序遵守 POSIX 的返回值规定吗？如果你的 main 函数返回了非 0 的数值，我们将认为程序报告了错误——在非法的输入上返回 0，以及在合法的输入上返回非 0 都将导致 Wrong Answer。
+* [ ] 程序够 roubust 吗？它会不会在一些非法的输入上 crash？如果系统里的进程很多呢？如果内存不够了呢？如果 open 或者 malloc 失败了呢？要知道，crash 一般是因为 undefined behavior (UB) 导致的——UB 没把所有的文件都删掉真是谢天谢地了。
+* [ ] 万一我得到进程号以后，进去发现文件没了 (进程终止了)，怎么办？会不会有这种情况？万一有我的程序会不会 crash……？
+* [ ] 进程的信息一直在变，文件的内容也一直在变 (两次 cat 的结果不同)。那我会不会读到不一致的信息(前一半是旧信息、新一半是新信息)？这两个问题都是 race condition 导致的；我们将会在并发部分回到这个话题。
--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,155 @@
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+typedef struct
+{
+  int pid;
+  int ppid;
+  int used;
+} pid_ppid_node;
+
+typedef struct
+{
+  int nodelen;
+  pid_ppid_node *nodelist;
+} pid_ppid_list;
+
+void print_usage(void);
+void dfs_build_tree(pid_ppid_list p_pp_list, int index, int father);
+
+// 打印 pid 树，递归思想
+void dfs_build_tree(pid_ppid_list p_pp_list, int index, int father)
+{
+  if (index == 0)
+  {
+    printf("0─┬─\n");
   }
-  assert(!argv[argc]);
+  if (p_pp_list.nodelen <= 0)
+  {
+    printf("pid_ppid_list nodelen errror , it`s too min,print tree error ! ! !");
+    return;
+  }
+  if (index == p_pp_list.nodelen)
+  {
+    return;
+  }
+
+  for (int i = 0; i < p_pp_list.nodelen; i++)
+  {
+    if (p_pp_list.nodelist[i].ppid == father && p_pp_list.nodelist[i].used == 0)
+    {
+      printf(" ├");
+      for (int j = 0; j < index - 1; j++)
+      {
+        printf("    ");
+      }
+      if (index == 1)
+      {
+        printf("────");
+      }
+      else
+      {
+        printf("└───");
+      }
+
+      printf("%d\n", p_pp_list.nodelist[i].pid);
+      p_pp_list.nodelist[i].used = 1;
+      dfs_build_tree(p_pp_list, index + 1, p_pp_list.nodelist[i].pid);
+      p_pp_list.nodelist[i].used = 0;
+    }
+  }
+  return;
+}
+
+/*
+  pid 和 ppid 获取并构建 pid_ppid_list
+*/
+pid_ppid_node get_pid_ppid(char filepath[])
+{
+  FILE *fp = NULL;
+  fp = fopen(filepath, "r");
+  char line[1024] = {0};
+  pid_ppid_node node;
+  // exit(0);
+  int count = 0;
+  if (!fp)
+  {
+    return node;
+  }
+  while (1)
+  {
+    if (EOF == fscanf(fp, "%s", line))
+    {
+      break;
+    }
+    if (strcmp(line, "Pid:") == 0)
+    {
+      count = count + 1;
+      fscanf(fp, "%s", line); // pid_no
+      node.pid = atoi(line);
+      fscanf(fp, "%s", line); // ppid
+      fscanf(fp, "%s", line); // ppid_no
+      node.ppid = atoi(line);
+    }
+  }
+  fclose(fp);
+  return node;
+};
+/*
+  根据 /proc 目录内容 寻找 相关进程信息
+*/
+pid_ppid_list traverse_dir(char *argv[])
+{
+  DIR *dp;
+  struct dirent *filename;
+  pid_ppid_list p_pp_list;
+  pid_ppid_node id_list[1024 * 10];
+  int index = 0;
+  dp = opendir("/proc");
+  if (!dp)
+  {
+    fprintf(stderr, "open directory error\n");
+    return p_pp_list;
+  }
+  while ((filename = readdir(dp)))
+  {
+    if (1 && (filename->d_name[0] >= '0' && filename->d_name[0] <= '9'))
+    {
+      char filepath[500] = "/proc/";
+      strcat(filepath, filename->d_name);
+      strcat(filepath, "/status");
+      id_list[index] = get_pid_ppid(filepath);
+      index += 1;
+    }
+  }
+  closedir(dp);
+  p_pp_list.nodelen = index;
+  p_pp_list.nodelist = id_list;
+  return p_pp_list;
+};
+
+int main(int argc, char *argv[])
+{
+  pid_ppid_list p_pp_list;
+
+  if (argc < 2 || strcmp(argv[1], "-V") == 0)
+  {
+    printf("Author: Build By XuXin \n"
+           "Version: 1.0 \n"
+           "Arguments: 1. -p  (--show-pid) build pstree order by pid\n"
+           "           2. -V version\n");
+    exit(0);
+  }
+  if (strcmp(argv[1], "-p") == 0 || strcmp(argv[1], "-P") == 0)
+  {
+    p_pp_list = traverse_dir(argv);
+    dfs_build_tree(p_pp_list, 0, 0);
+  }
+
   return 0;
 }
